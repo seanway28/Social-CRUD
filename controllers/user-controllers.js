4c98e339a12579ce453fe1c2148e6d2f280a068d
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-30 09:44:58 -0400

    user controllers

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -18,5 +18,61 @@ const userController = {
             });
     },
     // Get user by id /api/users/:id
-    getUserBy
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found by this id'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // Post new user /api/users
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    // Put to update user by id /api/users/:id
+    updateUser({ params, body }, res) {
+        User.findByIdAndUpdate({ _id: paramas.id }, body, {new: true, runValidators: true}
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found by this id'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+    // Delete to remove user by id /aapi/user/:id
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found by this id'});
+                    return;
+                } else if (dbUserData.thoughts) {
+                    return Thought.deleteMany(
+                        { _id: { $in: dbUserData.thoughts } }
+                    );
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+    // Friend Related Routes
+
+    // Post to add a new friend to a user;s friend list
 }
