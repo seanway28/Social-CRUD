9bfaa0007ab7a1cd0e689b47b9afe61a53c5472e
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-29 20:50:56 -0400

    models thought

---
+++ models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, model, Types } = require("mongoose");
+const date = require('../utils/date');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now,
+            get: (createdAtVal) => date(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
+
+const ThoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String, 
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => date(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
