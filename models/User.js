8d968bfd743076848fb394bcbcaa646e56e42909
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-09-02 16:41:06 -0400

    unable to find error. Insomnia fail #5

--- models/User.js
+++ models/User.js
@@ -17,13 +17,13 @@ const UserSchema = new Schema (
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
-                ref: 'Thought'
+                //ref: 'Thought'
             }
         ],
         friends: [
             {
                 type: Schema.Types.ObjectId,
-                ref: 'User'
+                //ref: 'User'
             }
         ]
     },
