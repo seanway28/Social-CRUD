1a887933501cf071d5f2c31d25b38493835a8320
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-09-05 13:21:08 -0400

    push per TA and AskBCS

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
-import mongoose from ('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Social-CRUD', {
-    useFindAndModify: false,
+    //useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 module.exports = mongoose.connection;
+
+// import mongoose = require('mongoose');
